8a1d3b89f06e0e7a2e8885420715216f28bb744e
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-10-19 15:53:00 -0300

    fix: middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -5,48 +5,39 @@ import { APP_ROUTES } from "./app/utils/app-routes.util";
 
 function isLogged() {
   const cookieStore = cookies();
-  const token = cookieStore.get("urbanvogue_session");
-
-  if (!token) {
-    return false;
-  }
-  return true;
+  return Boolean(cookieStore.get("urbanvogue_session"));
 }
 
-let routeHistory:any = [];
+const routeHistory: string[] = [];
 
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
   const pathname = url.pathname;
   const host = process.env.NEXT_PUBLIC_APP_URL;
 
-  console.log(pathname, isLogged());
+  const currentRoute = request.nextUrl.pathname;
+  routeHistory.push(currentRoute);
 
-   const currentRoute = request.nextUrl.pathname;
-   routeHistory.push(currentRoute);
+  if (routeHistory.length > 10) {
+    routeHistory.shift(); // Remove a rota mais antiga
+  }
 
-   if (routeHistory.length > 10) {
-     // Altere 10 para o número desejado de rotas a serem armazenadas
-     routeHistory.shift(); // Remove a rota mais antiga
-   }
+  const appPublicRoutes = new Set(Object.values(APP_ROUTES.public));
+  const appOthersRoutes = new Set(Object.values(APP_ROUTES.others));
 
-   console.log(routeHistory);
-   
-
-  const appPublicRoutes = Object.values(APP_ROUTES.public);
-  const isPublicRoute = appPublicRoutes.includes(pathname);
-
-  const appOthersRoutes = Object.values(APP_ROUTES.others);
-  const isOtherRoute = appOthersRoutes.includes(pathname);
+  const isPublicRoute = appPublicRoutes.has(pathname);
+  const isOtherRoute = appOthersRoutes.has(pathname);
 
   if (isOtherRoute && isLogged()) {
-    console.log("redirecionado a home");
-
+    console.log("Redirecionado para a home");
     return NextResponse.redirect(`${host}`);
   }
+
   if (!isPublicRoute && !isOtherRoute && !isLogged()) {
-    return NextResponse.redirect(`${host}login`);
+    console.log("Usuário não logado, redirecionando para login");
+    return NextResponse.redirect(`${host}/login`);
   }
+
   return NextResponse.next();
 }
 

6d5311a014fd7d8804d284e93c2c555bed10639a
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-10-19 15:48:30 -0300

    fix: remove dotenv import

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { config } from "dotenv";
-config();
 const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com", "localhost", "as1.ftcdn.net", "www.balancaservice.com.br", "static.ecosweb.com.br", "ph-cdn3.ecosweb.com.br", "example.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],

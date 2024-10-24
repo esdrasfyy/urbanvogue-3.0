318197c567918206f7542af284cdd287b39a6c72
esdrasfyy <contatoesdrasoficial@gmail.com>
2024-10-24 14:56:41 -0300

    fix: home & header

--- client/app/components/header/sub-components/menu-list.tsx
+++ client/app/components/header/sub-components/menu-list.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function MenuList() {
   return (
-    <ul className="flex gap-4 font-semibold tracking-widest">
+    <ul className="flex gap-4 font-semibold tracking-widest max-md:hidden">
       <li className="hover-effect hover-link">Inicio</li>
       <li className="hover-effect hover-link">Sobre</li>
       <li className="hover-effect hover-link">Contato</li>
--- client/app/components/home/grid-categories.tsx
+++ client/app/components/home/grid-categories.tsx
@@ -2,28 +2,32 @@ import React from "react";
 
 export function GridCategories() {
   return (
-    <section className="gridHome">
-      {" "}
-      <div className="grid-item-1 group duration-300 transition-all ease-linear overflow-hidden shadow-snipped rounded-md cursor-pointer">
-        <span className="absolute w-9 group-hover:-translate-x-8 writing uppercase flex items-center text-custom-textColor justify-center bg-custom-tertiaryBrand h-full group-hover:w-0 duration-300 transition-all ease-linear max-sm:hidden">Woman</span>
-        <div className="model1 group-hover:w-full duration-300 transition-all ease-linear overflow-hidden"></div>
-        <span className="absolute bottom-0 h-9 hidden items-center justify-center text-xl uppercase tracking-[0.2em] text-custom-textColor w-full group-hover:h-0 group-hover:translate-y-4 bg-custom-tertiaryBrand duration-300 transition-all ease-linear max-sm:flex">Woman</span>
+    <section className="h-[430px] w-full flex gap-6 text-2xl max-sm:flex-col max-sm:h-fit">
+      <a href="#" className="w-1/4 h-full relative overflow-hidden group max-sm:w-full max-sm:h-40">
+        <span className="max-sm:hidden absolute top-0 left-0 bg-custom-tertiaryBrand writing h-full w-8 rounded-l-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-w">Woman</span>
+        <div className="bg-[url('/images/fem-model.jpg')] bg-cover bg-center w-full h-full rounded-md shadow-md"></div>
+        <span className="hidden max-sm:inline absolute bottom-0 left-0 bg-custom-tertiaryBrand w-full h-8 rounded-b-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-h">Woman</span>
+      </a>
+
+      <div className="w-2/4 h-full flex flex-col gap-6 max-md:flex-row max-sm:flex-col max-sm:w-full">
+        <a href="#" className="w-full h-full relative overflow-hidden group max-sm:w-full max-sm:h-40">
+          <span className="hidden max-md:inline max-sm:hidden absolute top-0 left-0 bg-custom-tertiaryBrand writing h-full w-8 rounded-l-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-w">Children</span>
+          <div className="bg-[url('/images/kid-model.webp')] bg-cover bg-center w-full h-full rounded-md shadow-md"></div>
+          <span className="inline max-md:hidden max-sm:inline absolute bottom-0 left-0 bg-custom-tertiaryBrand w-full h-8 rounded-b-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-h">Children</span>
+        </a>
+
+        <a href="#" className="w-full h-full relative overflow-hidden group max-sm:w-full max-sm:h-40">
+          <span className="hidden max-md:inline max-sm:hidden absolute top-0 left-0 bg-custom-tertiaryBrand writing h-full w-8 rounded-l-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-w">Accessories</span>
+          <div className="bg-[url('/images/accessories-model.jpg')] bg-cover w-full h-full rounded-md shadow-md"></div>
+          <span className="inline max-md:hidden max-sm:inline absolute bottom-0 left-0 bg-custom-tertiaryBrand w-full h-8 rounded-b-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-h">Accessories</span>
+        </a>
       </div>
-      <div className="grid-item-2 group duration-300 transition-all ease-linear overflow-y-hidden shadow-snipped rounded-md cursor-pointer">
-        <span className="absolute left-0 w-9 group-hover:-translate-x-8 writing uppercase items-center text-custom-textColor justify-center bg-custom-tertiaryBrand h-full group-hover:w-0 duration-300 transition-all ease-linear hidden max-md:flex max-md:text-[1.4em] max-sm:hidden">children</span>
-        <div className="model2 group-hover:h-full duration-300 transition-all ease-linear overflow-hidden"></div>
-        <span className="absolute bottom-0 h-9 flex items-center justify-center text-xl uppercase tracking-[0.2em] text-custom-textColor w-full group-hover:h-0 group-hover:translate-y-4 bg-custom-tertiaryBrand duration-300 transition-all ease-linear max-md:hidden max-sm:flex">children</span>
-      </div>
-      <div className="grid-item-3 group duration-300 transition-all ease-linear overflow-y-hidden shadow-snipped rounded-md cursor-pointer">
-        <span className="absolute left-0 w-9 group-hover:-translate-x-8 writing uppercase items-center text-custom-textColor justify-center bg-custom-tertiaryBrand h-full group-hover:w-0 duration-300 transition-all ease-linear hidden max-md:flex max-md:text-[1.4em] max-sm:hidden">accessories</span>
-        <div className="model3 group-hover:h-full duration-300 transition-all ease-linear overflow-hidden"></div>
-        <span className="absolute bottom-0 h-9 flex items-center justify-center text-xl uppercase tracking-[0.2em] text-custom-textColor w-full group-hover:h-0 group-hover:translate-y-4 bg-custom-tertiaryBrand duration-300 transition-all ease-linear max-md:hidden max-sm:flex">accessories</span>
-      </div>
-      <div className="grid-item-4 group duration-300 transition-all ease-linear overflow-hidden shadow-snipped rounded-md cursor-pointer">
-        <span className="absolute left-0 w-9 group-hover:-translate-x-8 writing uppercase flex items-center text-custom-textColor justify-center bg-custom-tertiaryBrand h-full group-hover:w-0 duration-300 transition-all ease-linear max-sm:hidden">Man</span>
-        <div className="model4 group-hover:w-full duration-300 transition-all ease-linear overflow-hidden"></div>
-        <span className="absolute bottom-0 h-9 hidden items-center justify-center text-xl uppercase tracking-[0.2em] text-custom-textColor w-full group-hover:h-0 group-hover:translate-y-4 bg-custom-tertiaryBrand duration-300 transition-all ease-linear max-sm:flex">Man</span>
-      </div>
+
+      <a href="#" className="w-1/4 h-full relative overflow-hidden group max-sm:w-full max-sm:h-40">
+        <span className="max-sm:hidden absolute top-0 left-0 bg-custom-tertiaryBrand writing h-full w-8 rounded-l-md shadow-md text-center uppercase tracking-widest flex justify-center duration-300 group-hover-grid-w">Man</span>
+        <div className="bg-[url('/images/man-model.jpg')] bg-cover bg-center w-full h-full rounded-md shadow-md"></div>
+        <span className="hidden max-md:inline absolute bottom-0 left-0 bg-custom-tertiaryBrand w-full h-8 rounded-b-md shadow-md text-center uppercase tracking-widest duration-300 group-hover-grid-h">Man</span>
+      </a>
     </section>
   );
 }

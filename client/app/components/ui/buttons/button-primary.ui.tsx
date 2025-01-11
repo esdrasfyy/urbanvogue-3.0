fc80eb85c4913c14663efdbdd9faa26ca1297571
Fernando Esdras da Silva <fernaando.esdras@gmail.com>
2025-01-11 18:53:31 -0300

    fix: use onclick

--- client/app/components/ui/buttons/button-primary.ui.tsx
+++ client/app/components/ui/buttons/button-primary.ui.tsx
@@ -2,7 +2,7 @@ import { trans } from "@/app/libs/i18n.lib";
 
 export function ButtonPrimary({ classname, type, content, onclick }: { classname?: string; type: "button" | "submit"; content: string; onclick?: () => void}) {
   return (
-    <button type={type} className={`text-center block w-full rounded-md py-2 font-semibold border-custom-accentColor border hover-effect hover-bg-accent ${classname}`}>
+    <button onClick={onclick} type={type} className={`text-center block w-full rounded-md py-2 font-semibold border-custom-accentColor border hover-effect hover-bg-accent ${classname}`}>
       {trans.t(content)}
     </button>
   );
